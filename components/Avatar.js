abf36383bdb65f6a6b44348c9aee2417d065e5a3
fosahadal <fosahadal@protonmail.com>
2023-02-28 17:03:50 -0600

    post-cards

---
+++ components/Avatar.js
@@ -0,0 +1,11 @@
+export default function Avatar(){
+    return(
+        <div>
+        <img
+          alt="avatar"
+          class="h-12 w-12 rounded-full object-cover object-center"
+          src="https://images.unsplash.com/photo-1659757828854-0a0ebcf42ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80"
+        />
+      </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,20 @@
 
-import Card from "../components/Card";
-import NavigationCard from "../components/NavigationCard"
+import PostFormCard from "../components/PostFormCard";
+import NavigationCard from "../components/NavigationCard";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
   return (
     <div className="flex mt-4 max-w-4xl mx-auto gap-6">
-      <div className="w-1/3">
+      <div className="w-3/12">
         <NavigationCard>
           
         </NavigationCard>
       </div>
-      <div className="grow">
-        <Card>
-          from here
-        </Card>
-        <Card>
-          first post test
-        </Card>
+      <div className="9/12">
+        <PostFormCard>
+        </PostFormCard>
+        <PostCard />
       </div>
 
     </div>
