d9a12848982745196c88671aed7851388fd94b68
fosahadal <fosahadal@protonmail.com>
2023-03-02 16:55:09 -0600

    menu styled

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,11 +1,13 @@
 import Card from "./Card";
 
 export default function NavigationCard() {
+  const activeElement = 'flex gap-3 py-3 bg-socialBlue text-white -mx-10 px-10 rounded-md shadow-md shadow-gray-300';
+  const nonActiveElement = 'flex gap-3 py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 -mx-4 px-4 rounded-md transistion-all hover:scale-110 hover:shadow-md shadow-gray-300';
   return (
     <Card>
       <div className="px-4 py-2">
         <h2 className="text-gray-400 mb-3">Navigation</h2>
-        <a href="" className="flex gap-3 py-3">
+        <a href="" className={activeElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -22,7 +24,7 @@ export default function NavigationCard() {
           </svg>
           Home
         </a>
-        <a href="" className="flex gap-3 py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -39,7 +41,7 @@ export default function NavigationCard() {
           </svg>
           Friends
         </a>
-        <a href="" className="flex gap-3 py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -56,7 +58,7 @@ export default function NavigationCard() {
           </svg>
           Saved posts
         </a>
-        <a href="" className="flex gap-3 py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -73,7 +75,7 @@ export default function NavigationCard() {
           </svg>
           Notifications
         </a>
-        <a href="" className="flex gap-3 py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
